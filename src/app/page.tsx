0579194621ae5fc9ee6ed5596c36134c23cc3aad
Kadu <kadu.silva2014@gmail.com>
2023-12-01 06:45:17 -0300

    ✨ feat: configure dark theme
    configure dark theme using shadcn ui and tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { ModeToggle } from "@/components/Theme/ThemeToggle";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World!!</h1>
+
+      <ModeToggle />
     </main>
   )
 }
